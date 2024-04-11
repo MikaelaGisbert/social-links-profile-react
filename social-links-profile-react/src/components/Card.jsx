35e1cb0677971f5f717941e618fe8217f1894518
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-04-11 11:48:27 -0400

    clase 11/04/2024

--- social-links-profile-react/src/components/Card.jsx
+++ social-links-profile-react/src/components/Card.jsx
@@ -1,18 +1,18 @@
-import React from "react"
+import React from "react";
 // import avatarJessica from "../public/avatar-jessica.jpeg"
-import UserDetails from "./UserDetails"
-import ButtonSection from "./ButtonSection"
+import UserDetails from "./UserDetails";
+import ButtonSection from "./ButtonSection";
 import "./Card.css";
 
-function Card() {
-
+function Card(props) {
+  const user = props.user;
+  console.log(user);
   return (
     <div className="contenedorDinamico">
-      <UserDetails/>
-      <ButtonSection/>
+      {/* <UserDetails user = {user}/> */}
+      <ButtonSection />
     </div>
-
-  )
+  );
 }
 
 export default Card;
