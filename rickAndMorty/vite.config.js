295fd110c1859c373b50ce923a7419071af92297
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-04-25 01:18:16 -0400

    Fin

--- rickAndMorty/vite.config.js
+++ rickAndMorty/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mikaelagisbert.github.io/social-links-profile-react/rickAndMorty/",
+  base: "https://mikaelagisbert.github.io/social-links-profile-react/",
 });
